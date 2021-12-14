8bd3ddeb055e02824348fdfba4869ebfbe684fd0
Bdani2201 <bdani2201@gmail.com>
2021-12-14 11:21:49 +0100

    2. feladat

--- 2021-12-14_Születések/Program.cs
+++ 2021-12-14_Születések/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.IO;
 namespace _2021_12_14_Születések
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Személy> személyek = new List<Személy>();
+
+            foreach (var sor in File.ReadAllLines("vas.txt"))
+            {
+                személyek.Add(new Személy(sor));
+            }
+
+            Console.WriteLine(személyek.Count);
         }
     }
+
+    
 }
---
+++ 2021-12-14_Születések/Személy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2021_12_14_Születések
+{
+    class Személy
+    {
+        public string SzemélyiSzám { get; set; }
+
+        public Személy(string sor)
+        {
+            SzemélyiSzám = sor;
+        }
+    }
+}
